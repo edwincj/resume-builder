57abd270967accbefe76e40a6107bde9e741bfab
edwincj <edwincj1998@gmail.com>
2022-02-13 01:49:43 +0530

    Added base name prop for router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App current="edit" />} />
         <Route path="/view" element={<App current="view" />} />
