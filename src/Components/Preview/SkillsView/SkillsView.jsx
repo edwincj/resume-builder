436f19e871a8b7fdc85924a8d9762ed78efdadb2
edwincj <edwincj1998@gmail.com>
2022-02-13 01:40:39 +0530

    Added useref for preventing dynamic generation of uuid

--- src/Components/Preview/SkillsView/SkillsView.jsx
+++ src/Components/Preview/SkillsView/SkillsView.jsx
@@ -2,8 +2,11 @@ import "./SkillsView.css";
 
 import { Badge } from "react-bootstrap";
 import { v4 as uuidv4 } from "uuid";
+import { useRef } from "react";
 
 const SkillsView = ({ data }) => {
+  const uuidList = useRef(Array.from({ length: data.length }, () => uuidv4()));
+
   return (
     <div className="preview-section">
       <div className="side-bar">
@@ -11,8 +14,13 @@ const SkillsView = ({ data }) => {
       </div>
       <div className="skill-content">
         {data.length > 0 ? (
-          data.map(skill => (
-            <Badge pill bg="secondary" key={uuidv4()} className="skill-label">
+          data.map((skill, index) => (
+            <Badge
+              pill
+              bg="secondary"
+              key={uuidList.current[index]}
+              className="skill-label"
+            >
               {skill.label}
             </Badge>
           ))
